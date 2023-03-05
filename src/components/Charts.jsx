6d2f44450c87c34585df24f8b5f8e0377d48f3e7
CodexLegacy <73387073+CodexLegacy@users.noreply.github.com>
2023-03-05 10:45:08 +0100

    Almost done, just the donut pie chart in chart.js remaining and minor fixes

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import "../assets/css/Charts.scss"
 import Chart from 'react-apexcharts';
+import SmallerMap from './SmallerMap';
+import MaleIcon from "../assets/images/male.png"
+import FemaleIcon from "../assets/images/female.png"
 
 const PieChart = () => {
   return (
     <section className='charts'>
       <div className="chart-wrapper">
         <div className="donut-chart inner">
-          <Chart
+          {/* <Chart
             type="donut"
             width= {382.61}
             height= {322.61} 
@@ -24,10 +27,87 @@ const PieChart = () => {
                 }
               }
             }}
-          />
+          /> */}
         </div>
-      <div className="map-chart inner">2</div>
-      <div className="info inner">3</div>
+
+        <div className="map-chart inner">
+          <h6>CANDIDATES BY GEOPOLITICAL ZONES</h6>
+          <div className="map-info">
+            <SmallerMap />
+            <div className="region-info">
+              <div className="info">
+                <span className='info-green multi'></span>
+                <p>North East</p>
+                <div className="number">
+                  <span>3000</span>
+                </div>
+              </div>
+
+              <div className="info">
+                <span className='info-gray multi'></span>
+                <p>North West</p>
+                <div className="number">
+                  <span>3000</span>
+                </div>
+              </div>
+
+              <div className="info">
+                <span className='info-puple multi'></span>
+                <p>North Central</p>
+                <div className="number">
+                  <span>3000</span>
+                </div>
+              </div>
+
+              <div className="info">
+                <span className='info-red multi'></span>
+                <p>South West</p>
+                <div className="number">
+                  <span>3000</span>
+                </div>
+              </div>
+
+              <div className="info">
+                <span className='info-blue multi'></span>
+                <p>South South</p>
+                <div className="number">
+                  <span>3000</span>
+                </div>
+              </div>
+
+              <div className="info">
+                <span className='info-cyn multi'></span>
+                <p>South East</p>
+                <div className="number">
+                  <span>3000</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="gender-info inner">
+          <div className="right-info">
+            <h6>TOTAL ELECTED MEMBERS</h6>
+            <p>10,000</p>
+          </div>
+
+          <div className="right-info">
+            <h6>
+              MALE
+              <img src={MaleIcon} alt="MaleIcon" />
+            </h6>
+            <p>9,000</p>
+          </div>
+
+          <div className="right-info">
+            <h6>
+              FEMALE
+              <img src={FemaleIcon} alt="FemaleIcon" />
+            </h6>
+            <p>1,000</p>
+          </div>
+        </div>
       </div>
     </section>
   )
